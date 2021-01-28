a79d19932e794acc6e9808167a24131bc1c193ba
Ihcen <ihcen>
2021-01-29 00:20:09 +0100

    init db

---
+++ emarket-reviews/test/models/avis_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class AvisTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ emarket-reviews/test/models/enseigne_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class EnseigneTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
